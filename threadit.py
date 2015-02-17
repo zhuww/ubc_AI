51e4bc26999ab514afc41a5d6e3c396ca2c58eb4
Weiwei Zhu <zhuwwpku@gmail.com>
2015-02-17 12:02:22 +0100

    update threadit function and README.

--- threadit.py
+++ threadit.py
@@ -1,21 +1,34 @@
+"""
+A module for threading computation on a multi-cpu machine
+
+by Weiwei Zhu
+June 2013
+"""
 import multiprocessing as MP
 import sys
 import traceback
-num_workers = max(1, MP.cpu_count() - 1)
-def threadit(func, arglist, OnOffSwitch={'state':False}):
+num_cpus = max(1, MP.cpu_count() - 1)
+
+def threadit(func, arglist, OnOffSwitch={'state':False}, num_threads=20):
     """
     A wrapper for multi-threading any function (func) given a argument list (arglist). The OnOffSwitch is a flag that got set to True when a progress is already running in a thread. It would not spam more threads when the flag is set to True.
     """
+    num_workers = min(num_threads, num_cpus)
     def worker(q,retq, pipe, func, arglist):
         while True:
             idx = q.get()
             if idx is not None:
-                retq.put({idx:func(*(arglist[idx]))})
+                try:
+                    retq.put({idx:func(*(arglist[idx]))})
+                except:
+                    except_type, except_class, tb = sys.exc_info()
+                    pipe.send((except_type, except_class, traceback.extract_tb(tb)))
+
+                    retq.put(None)
             else:
                 break
             q.task_done()
         q.task_done()
-    #print func.__name__, ' OnOffSwitch:', OnOffSwitch['state'], len(arglist)
     if OnOffSwitch['state'] == False or len(arglist) <=3:
         #if no threading is already running or the number of jobs to spaw is smaller than 3, don't thread it.
         OnOffSwitch['state'] = True
